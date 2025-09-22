b7bb08c2b6a585c6b3151a92f7ddd69dbe6c3551
ndinani <ndinani@protonmail.com>
2025-09-22 15:21:22 +0200

    Add factorial calculator, ASAP frontend exercise, and index HTML file

---
+++ javascript/14.asapFrontend.js
@@ -0,0 +1,20 @@
+// Exercise from Asap Front End JS Tutorial.
+for (let i = 1; i <= 15; ++i){
+    if (i % 5 === 0) {
+        console.log("ASAP FRONT END")
+    }
+
+    else if(i%2 === 0) {
+        console.log("FrontEnd")
+    }
+    
+    else console.log("ASAP")
+ 
+}
+
+
+let str = "Asap Front End";
+for (let i = 0; i <= str.length; ++i){
+    console.log(str[i])
+}
+
