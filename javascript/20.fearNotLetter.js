05ddbf445e71904e8088ccd36a77ffbf95a8ff1e
ndinani <ndinani@protonmail.com>
2025-10-07 10:40:26 +0200

    Implement fearNotLetter function to identify missing letters and update index.html to reference the new script

---
+++ javascript/20.fearNotLetter.js
@@ -0,0 +1,27 @@
+
+function fearNotLetter(string){
+
+    // Range of valid letters
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+
+    if (letters.includes(string)){
+        return undefined;
+    }
+
+
+    
+
+    
+}
+
+// Process: we want to process a given string and return a missing letter
+// Expectation: alphabetical order of string
+// If not, return the index where expectation is not true
+
+// with each instance of iteration, return index of iteration
+
+let letters = "abcdefghijklmnopqrstuvwxyz";
+let string = "abcdefgi"
+let start = letters.indexOf(string)
+
+console.log(expected)
