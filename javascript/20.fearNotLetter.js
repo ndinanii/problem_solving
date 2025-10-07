83b8c226573fe31a0b46dab11cf8850f130fdfdf
ndinani <ndinani@protonmail.com>
2025-10-07 11:25:13 +0200

    Refactor fearNotLetter function for clarity and correctness

--- javascript/20.fearNotLetter.js
+++ javascript/20.fearNotLetter.js
@@ -1,27 +1,28 @@
+function fearNotLetter(str) {  
+    // Create a string of all letters in order  
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-function fearNotLetter(string){
+    // Find where our string starts in the alphabet  
+    const startIndex = alphabet.indexOf(str[0]);  
 
-    // Range of valid letters
-    let letters = "abcdefghijklmnopqrstuvwxyz";
-
-    if (letters.includes(string)){
-        return undefined;
-    }
-
-
+    // Check each character in order  
+    for (let i = 0; i < str.length; i++) {
+        // The letter that should be at this position    
+        const expected = alphabet[startIndex + i];
+        
+        // The actual letter from our string
+        const actual = str[i];        
+        
+        // If they don't match, we found the missing letter
     
+        if (expected !== actual) {
+            return expected;    
+        }  
+    }  
 
+    // All letters are in order  
+    return undefined;
     
 }
 
-// Process: we want to process a given string and return a missing letter
-// Expectation: alphabetical order of string
-// If not, return the index where expectation is not true
-
-// with each instance of iteration, return index of iteration
-
-let letters = "abcdefghijklmnopqrstuvwxyz";
-let string = "abcdefgi"
-let start = letters.indexOf(string)
-
-console.log(expected)
+console.log(fearNotLetter("abcefg"))
